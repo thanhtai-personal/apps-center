3906a2c6eeb47c600394d0d38b09676b9bdaa65e
raidenlight170 <lightraiden170@gmail.com>
2024-09-27 14:46:48 +0700

    Update useCrawler.ts

--- client/builtIn/react-jobs-listing/src/hooks/useCrawler.ts
+++ client/builtIn/react-jobs-listing/src/hooks/useCrawler.ts
@@ -4,12 +4,16 @@ import { JobsListingSDK } from "@core-sdk/jobs-listing";
 
 
 export const useCrawler = () => {
-  const { jobStore } = useJobsListingStore();
+  const { jobStore, notiStore } = useJobsListingStore();
 
   const exportAnydayJob = async (jobId: string, categoryId: number, htmlString: string) => {
     try {
       const jobs: any = await JobsListingSDK.getInstance().exportAnydayJob(jobId, categoryId, htmlString);
-      jobStore.jobs = jobs; 
+      jobStore.jobs = jobs;
+      notiStore.messageQueue?.push({
+        children: "Add job success!",
+        variant: "success"
+      })
     } catch (error) { }
   }
 
