1be20e717304821cc0fe5ee27bafeb375b119d68
raidenlight170 <lightraiden170@gmail.com>
2024-10-01 01:13:50 +0700

    Update index.tsx

--- apps/jobs-listing/src/components/admin/login/index.tsx
+++ apps/jobs-listing/src/components/admin/login/index.tsx
@@ -1,16 +1,40 @@
 import { PAGE_MAX_WIDTH } from "@/utils/constants";
 import { observer } from "@core-ui/react-mobx-state";
-import { Flex } from "@core-ui/react-mui-core";
-import { LoginForm } from "../../login/LoginForm";
+import { Grid } from "@core-ui/react-mui-core/materials"
+import { Flex, LazyImage } from "@core-ui/react-mui-core";
 import recruiterBg from "@/assets/images/bg/recruiter_bg.png"
+import Carousel from 'react-material-ui-carousel';
+import jobspage from "@/assets/images/bg/jobspage.png"
+import novels_page from "@/assets/images/bg/novels_page.png"
+import portfolio_preview from "@/assets/images/bg/portfolio_preview.png"
+import performanceResult from "@/assets/images/bg/performance-result.png"
+import { Link } from "@core-ui/react-core";
+import { LoginForm } from "@/components/login/LoginForm";
 
-export const AdminLoginComponent = observer(() => {
+const images = [
+  {
+    src: portfolio_preview,
+    url: "/me"
+  }, {
+    src: jobspage,
+    url: "/jobs"
+  }, {
+    src: novels_page,
+    url: "/novels"
+  }, {
+    src: performanceResult,
+    url: "https://pagespeed.web.dev/analysis/https-my-projects-delta-orcin-vercel-app-me/vcos3fthy7?form_factor=desktop"
+  }
+];
+
+export const LoginComponent = observer(() => {
 
   return (
     <Flex fullWidth center className="login_page">
-      <Flex bgcolor={"red"}
+      <Flex
         width={"100vw"}
         height={"100vh"}
+        center
         style={{
           backgroundImage: `url(${recruiterBg})`,
           backgroundSize: "cover",
@@ -19,6 +43,44 @@ export const AdminLoginComponent = observer(() => {
         }}
       >
         <Flex fullWidth maxWidth={PAGE_MAX_WIDTH} column>
+          <Carousel
+            animation={"slide"}
+            interval={10000}
+            swipe
+            fullHeightHover
+            navButtonsWrapperProps={{
+              style: { margin: "0 10px" },
+            }}
+          >
+            {images.map((image, index) => (
+              <Flex key={index} fullSize>
+                <LazyImage
+                  src={image.src}
+                  alt={`Slide ${index + 1}`}
+                  style={{ width: "100%", height: "100%", objectFit: "cover", maxWidth: PAGE_MAX_WIDTH, borderRadius: 16 }}
+                  imgStyle={{ width: "100%", height: "100%", objectFit: "cover", maxWidth: PAGE_MAX_WIDTH, borderRadius: 16 }}
+                />
+              </Flex>
+            ))}
+          </Carousel>
+          <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 12, sm: 12, md: 12 }}>
+            {images.map((image, index) => {
+              return (
+                <Grid item xs={12} key={index} sm={6} md={3}>
+                  <Link to={image.url} target="_blank">
+                    <Flex fullSize cursorPointer>
+                      <LazyImage
+                        src={image.src}
+                        alt={`Slide ${index + 1}`}
+                        style={{ width: "100%", height: "100%", objectFit: "cover", maxWidth: PAGE_MAX_WIDTH, borderRadius: 16 }}
+                        imgStyle={{ width: "100%", height: "100%", objectFit: "cover", maxWidth: PAGE_MAX_WIDTH, borderRadius: 16 }}
+                      />
+                    </Flex>
+                  </Link>
+                </Grid>
+              )
+            })}
+          </Grid>
         </Flex>
       </Flex>
       <LoginForm isAdmin />
