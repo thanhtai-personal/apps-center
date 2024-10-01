c98502fd77ad324761c0254c49247410275f9775
raidenlight170 <thanhtai.tttgalaxy@gmail.com>
2024-10-01 16:12:20 +0700

    update home page

--- apps/jobs-listing/src/components/portfolio/Contact.tsx
+++ apps/jobs-listing/src/components/portfolio/Contact.tsx
@@ -15,7 +15,7 @@ export const Contact = observer(() => {
   const { getText } = useLanguage(contactLangObj)
 
   return (
-    <Flex fullWidth mt={12} column center>
+    <Flex fullWidth mt={2} column center>
       <Flex fullWidth height={1200} position={"relative"}>
         {tabletSizeDown ? <Flex fullSize alignItems={"flex-end"}>
           <img src={reflectBg} alt="water reflect background" style={{
@@ -44,10 +44,157 @@ export const Contact = observer(() => {
                     WebkitBackdropFilter: "blur(2px)"
                   }}
                 >
-                  <Flex fullWidth column={tabletSizeDown} center={tabletSizeDown}
+                  <Flex fullWidth column center={tabletSizeDown}
                     justifyContent={tabletSizeDown ? "center" : "space-between"}
                   >
-                    <Flex column>
+                    <Flex column center>
+                      <Text className={globalStyles.textOrbiBold24} color={"#ffffff"}
+                        textTransform={"uppercase"}
+                        textAlign={"center"}
+                      >
+                        {getText("Projects")}
+                      </Text>
+                      <Flex fullWidth border={"solid 2px rgba(255,255,255, 0.1)"} mt={1} mb={2}></Flex>
+                      <Flex fullWidth center column={tabletSizeDown}>
+                        <Flex flex={1} px={2} column
+                          alignItems={tabletSizeDown ? "center" : "flex-end"}>
+                          <Flex my={0.5}></Flex>
+                          <Animates.RippleButton id="project-1"
+                            style={{ padding: "8px 16px", borderRadius: "8px" }}
+                          >
+                            <Link to="https://www.taitran.dev/hptool" target="_blank">
+                              <Text className={globalStyles.textKanit18} color={"#cccccc"}
+                                textTransform={"uppercase"}
+                              >
+                                {getText("HP-Tools")}
+                              </Text>
+                            </Link>
+                          </Animates.RippleButton>
+
+                          <Flex my={0.5}></Flex>
+                          <Animates.RippleButton id="project-2"
+                            style={{ padding: "8px 16px", borderRadius: "8px" }}
+                          >
+                            <Link to="https://www.taitran.dev/me" target="_blank">
+                              <Text className={globalStyles.textKanit18} color={"#cccccc"}
+                                textTransform={"uppercase"}
+                              >
+                                {getText("Portfolio page")}
+                              </Text>
+                            </Link>
+                          </Animates.RippleButton>
+
+                          <Flex my={0.5}></Flex>
+                          <Animates.RippleButton id="project-3"
+                            style={{ padding: "8px 16px", borderRadius: "8px" }}
+                          >
+                            <Link to="https://www.taitran.dev/novels" target="_blank">
+                              <Text className={globalStyles.textKanit18} color={"#cccccc"}
+                                textTransform={"uppercase"}
+                              >
+                                {getText("Novels online page")}
+                              </Text>
+                            </Link>
+                          </Animates.RippleButton>
+
+                          <Flex my={0.5}></Flex>
+                          <Animates.RippleButton id="project-4"
+                            style={{ padding: "8px 16px", borderRadius: "8px" }}
+                          >
+                            <Link to="https://www.taitran.dev/jobs" target="_blank">
+                              <Text className={globalStyles.textKanit18} color={"#cccccc"}
+                                textTransform={"uppercase"}
+                              >
+                                {getText("Jobs Seeker online page")}
+                              </Text>
+                            </Link>
+                          </Animates.RippleButton>
+
+                          <Flex my={0.5}></Flex>
+                          <Animates.RippleButton id="project-5"
+                            style={{ padding: "8px 16px", borderRadius: "8px" }}
+                          >
+                            <Link to="https://www.taitran.dev/soccer-scores" target="_blank">
+                              <Text className={globalStyles.textKanit18} color={"#cccccc"}
+                                textTransform={"uppercase"}
+                              >
+                                {getText("Soccer score online page")}
+                              </Text>
+                            </Link>
+                          </Animates.RippleButton>
+                        </Flex>
+
+                        <Flex flex={1} px={2} column alignItems={tabletSizeDown ? "center" : "flex-start"}>
+
+                          <Flex my={0.5}></Flex>
+                          <Animates.RippleButton id="project-8"
+                            style={{ padding: "8px 16px", borderRadius: "8px" }}
+                          >
+                            <Link to="https://www.taitran.dev/smart-docs" target="_blank">
+                              <Text className={globalStyles.textKanit18} color={"#cccccc"}
+                                textTransform={"uppercase"}
+                              >
+                                {getText("Smart-docs")}
+                              </Text>
+                            </Link>
+                          </Animates.RippleButton>
+
+                          <Flex my={0.5}></Flex>
+                          <Animates.RippleButton id="project-9"
+                            style={{ padding: "8px 16px", borderRadius: "8px" }}
+                          >
+                            <Link to="https://www.taitran.dev/universe" target="_blank">
+                              <Text className={globalStyles.textKanit18} color={"#cccccc"}
+                                textTransform={"uppercase"}
+                              >
+                                {getText("Portfolio-v2")}
+                              </Text>
+                            </Link>
+                          </Animates.RippleButton>
+
+                          <Flex my={0.5}></Flex>
+                          <Animates.RippleButton id="project-10"
+                            style={{ padding: "8px 16px", borderRadius: "8px" }}
+                          >
+                            <Link to="https://www.taitran.dev/portfolio" target="_blank">
+                              <Text className={globalStyles.textKanit18} color={"#cccccc"}
+                                textTransform={"uppercase"}
+                              >
+                                {getText("Portfolio-v1")}
+                              </Text>
+                            </Link>
+                          </Animates.RippleButton>
+
+                          <Flex my={0.5}></Flex>
+                          <Animates.RippleButton id="project-6"
+                            style={{ padding: "8px 16px", borderRadius: "8px" }}
+                          >
+                            <Link to="https://www.taitran.dev/landing-templates" target="_blank">
+                              <Text className={globalStyles.textKanit18} color={"#cccccc"}
+                                textTransform={"uppercase"}
+                              >
+                                {getText("Landing page templates")}
+                              </Text>
+                            </Link>
+                          </Animates.RippleButton>
+
+                          <Flex my={0.5}></Flex>
+                          <Animates.RippleButton id="project-7"
+                            style={{ padding: "8px 16px", borderRadius: "8px" }}
+                          >
+                            <Link to="https://www.taitran.dev/dev-tech-landing" target="_blank">
+                              <Text className={globalStyles.textKanit18} color={"#cccccc"}
+                                textTransform={"uppercase"}
+                              >
+                                {getText("DEV.TECT Landing page")}
+                              </Text>
+                            </Link>
+                          </Animates.RippleButton>
+                        </Flex>
+                      </Flex>
+                    </Flex>
+
+                    <Flex column center mt={16}>
                       <Text className={globalStyles.textOrbiBold24} color={"#ffffff"}
                         textTransform={"uppercase"}
                       >
@@ -55,10 +202,7 @@ export const Contact = observer(() => {
                       </Text>
                       <Flex fullWidth border={"solid 2px rgba(255,255,255, 0.1)"} mt={1} mb={2}></Flex>
 
-                      <Flex mt={2} column
-                        // justifyContent={tabletSizeDown ? "center" : "flex-start"}
-                        alignItems={tabletSizeDown ? "center" : "flex-start"}
-                      >
+                      <Flex mt={2} column center>
                         <Text className={globalStyles.textKanit18} color={"#777777"}
                           textTransform={"uppercase"}
                         >
@@ -66,151 +210,11 @@ export const Contact = observer(() => {
                         </Text>
                         <Text className={globalStyles.textKanit18} color={"#777777"}
                           textTransform={"uppercase"}
+                          textAlign={"center"}
                         >
                           {getText("TEL: 0972828264")}
                         </Text>
                       </Flex>
-                    </Flex>
-
-                    <Flex column
-                      // justifyContent={tabletSizeDown ? "center" : "flex-end"}
-                      alignItems={tabletSizeDown ? "center" : "flex-end"}
-                    >
-                      <Text className={globalStyles.textOrbiBold24} color={"#ffffff"}
-                        textTransform={"uppercase"}
-                      >
-                        {getText("Projects")}
-                      </Text>
-                      <Flex fullWidth border={"solid 2px rgba(255,255,255, 0.1)"} mt={1} mb={2}></Flex>
-                      <Animates.RippleButton id="project-2"
-                        style={{ padding: "8px 16px", borderRadius: "8px" }}
-                      >
-                        <Link to="https://www.taitran.dev/me" target="_blank">
-                          <Text className={globalStyles.textKanit18} color={"#cccccc"}
-                            textTransform={"uppercase"}
-                          >
-                            {getText("Portfolio page")}
-                          </Text>
-                        </Link>
-                      </Animates.RippleButton>
-
-                      <Flex my={0.5}></Flex>
-                      <Animates.RippleButton id="project-3"
-                        style={{ padding: "8px 16px", borderRadius: "8px" }}
-                      >
-                        <Link to="https://www.taitran.dev/novels" target="_blank">
-                          <Text className={globalStyles.textKanit18} color={"#cccccc"}
-                            textTransform={"uppercase"}
-                          >
-                            {getText("Novels online page")}
-                          </Text>
-                        </Link>
-                      </Animates.RippleButton>
-
-                      <Flex my={0.5}></Flex>
-                      <Animates.RippleButton id="project-4"
-                        style={{ padding: "8px 16px", borderRadius: "8px" }}
-                      >
-                        <Link to="https://www.taitran.dev/jobs" target="_blank">
-                          <Text className={globalStyles.textKanit18} color={"#cccccc"}
-                            textTransform={"uppercase"}
-                          >
-                            {getText("Jobs Seeker online page")}
-                          </Text>
-                        </Link>
-                      </Animates.RippleButton>
-
-                      <Flex my={0.5}></Flex>
-                      <Animates.RippleButton id="project-5"
-                        style={{ padding: "8px 16px", borderRadius: "8px" }}
-                      >
-                        <Link to="https://www.taitran.dev/soccer-scores" target="_blank">
-                          <Text className={globalStyles.textKanit18} color={"#cccccc"}
-                            textTransform={"uppercase"}
-                          >
-                            {getText("Soccer score online page")}
-                          </Text>
-                        </Link>
-                      </Animates.RippleButton>
-
-                      <Flex my={0.5}></Flex>
-                      <Animates.RippleButton id="project-6"
-                        style={{ padding: "8px 16px", borderRadius: "8px" }}
-                      >
-                        <Link to="https://www.taitran.dev/landing-templates" target="_blank">
-                          <Text className={globalStyles.textKanit18} color={"#cccccc"}
-                            textTransform={"uppercase"}
-                          >
-                            {getText("Landing page templates")}
-                          </Text>
-                        </Link>
-                      </Animates.RippleButton>
-
-                      <Flex my={0.5}></Flex>
-                      <Animates.RippleButton id="project-7"
-                        style={{ padding: "8px 16px", borderRadius: "8px" }}
-                      >
-                        <Link to="https://www.taitran.dev/dev-tech-landing" target="_blank">
-                          <Text className={globalStyles.textKanit18} color={"#cccccc"}
-                            textTransform={"uppercase"}
-                          >
-                            {getText("DEV.TECT Landing page")}
-                          </Text>
-                        </Link>
-                      </Animates.RippleButton>
-
-                      <Flex my={0.5}></Flex>
-                      <Animates.RippleButton id="project-8"
-                        style={{ padding: "8px 16px", borderRadius: "8px" }}
-                      >
-                        <Link to="https://www.taitran.dev/smart-docs" target="_blank">
-                          <Text className={globalStyles.textKanit18} color={"#cccccc"}
-                            textTransform={"uppercase"}
-                          >
-                            {getText("Smart-docs")}
-                          </Text>
-                        </Link>
-                      </Animates.RippleButton>
-
-                      <Flex my={0.5}></Flex>
-                      <Animates.RippleButton id="project-9"
-                        style={{ padding: "8px 16px", borderRadius: "8px" }}
-                      >
-                        <Link to="https://www.taitran.dev/hptool" target="_blank">
-                          <Text className={globalStyles.textKanit18} color={"#cccccc"}
-                            textTransform={"uppercase"}
-                          >
-                            {getText("HP-Tools")}
-                          </Text>
-                        </Link>
-                      </Animates.RippleButton>
-
-                      <Flex my={0.5}></Flex>
-                      <Animates.RippleButton id="project-9"
-                        style={{ padding: "8px 16px", borderRadius: "8px" }}
-                      >
-                        <Link to="https://www.taitran.dev/universe" target="_blank">
-                          <Text className={globalStyles.textKanit18} color={"#cccccc"}
-                            textTransform={"uppercase"}
-                          >
-                            {getText("Portfolio-v2")}
-                          </Text>
-                        </Link>
-                      </Animates.RippleButton>
-
-                      <Flex my={0.5}></Flex>
-                      <Animates.RippleButton id="project-10"
-                        style={{ padding: "8px 16px", borderRadius: "8px" }}
-                      >
-                        <Link to="https://www.taitran.dev/portfolio" target="_blank">
-                          <Text className={globalStyles.textKanit18} color={"#cccccc"}
-                            textTransform={"uppercase"}
-                          >
-                            {getText("Portfolio-v1")}
-                          </Text>
-                        </Link>
-                      </Animates.RippleButton>
-
                     </Flex>
                   </Flex>
 
