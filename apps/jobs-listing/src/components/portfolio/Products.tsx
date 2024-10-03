b80ad87f28926942769fdcea1b0c20c32863953c
raidenlight170 <thanhtai.tttgalaxy@gmail.com>
2024-10-04 00:20:57 +0700

    update animation footer

--- apps/jobs-listing/src/components/portfolio/Products.tsx
+++ apps/jobs-listing/src/components/portfolio/Products.tsx
@@ -1,4 +1,5 @@
 import "@core-ui/react-animates/dist/bgClouds.style.css"
+// import "@core-ui/react-animates/dist/meteor/WaterMeteor.style.css"
 
 import cloudBg1 from "@/assets/images/bg/clouds_1.png"
 import cloudBg2 from "@/assets/images/bg/clouds_2.png"
@@ -7,13 +8,23 @@ import skillsetPng from "@/assets/images/skillset.svg"
 import smokePng from "@/assets/images/smoke.png"
 import pageSpeedRsImg from "@/assets/images/performance-result.png"
 
-import { Animates } from "@core-ui/react-animates";
+import { Animates, GAME } from "@core-ui/react-animates";
 import { observer } from "@core-ui/react-mobx-state";
 import { Flex, LazyImage, Text, useResponsive } from "@core-ui/react-mui-core";
 import { IntersectionObserverView } from "@core-ui/react-viewframe";
 import { useGlobalStyles } from "@/styles/globalStyle";
 import { useLanguage } from "@/hooks/useLanguage";
 import { Link } from "@core-ui/react-core";
+import shootAudio from "@/assets/game/sound/shoot.mp3";
+import pingAudio from "@/assets/game/sound/ping.mp3";
+import waterAudio from "@/assets/game/sound/water.mp3";
+import cavePng from "@/assets/game/images/cave.png";
+import sonaPng from "@/assets/game/images/sonar.png";
+import clamGif from "@/assets/game/images/clam.gif";
+import urchinGif from "@/assets/game/images/urchin.gif";
+import whaleGif from "@/assets/game/images/whale.gif";
+import subGif from "@/assets/game/images/sub.gif";
+import wall1Jpg from "@/assets/game/images/wall1.jpg";
 
 export const Products = observer(() => {
   const { tabletSizeDown, md1160 } = useResponsive({ md1160: 1160 });
@@ -49,71 +60,70 @@ export const Products = observer(() => {
                 : globalStyles.textOrbiBold32}>{getText("Products")}</Text>
             </Flex>
             <Flex fullWidth column mb={tabletSizeDown ? 2 : 6} mt={4}>
-              <Flex fullWidth minHeight={250}>
-                <IntersectionObserverView>
-                  <Flex fullWidth px={2}>
-                    <Animates.GrowUpAppear>
-                      <Flex column
-                        bgcolor={"#000"}
-                        boxShadow={"0 2px 5px rgb(55, 120, 251,0.3)"}
-                        borderRadius={"16px"}
-                        overflow={"hidden"}
-                        minHeight={250}
-                        p={1}
-                        py={4}
-                        fullWidth
+              <Flex fullWidth px={2}>
+                <Flex column
+                  bgcolor={"#000"}
+                  borderRadius={"16px"}
+                  overflow={"hidden"}
+                  p={1}
+                  py={4}
+                  fullWidth
+                  style={{
+                    backdropFilter: "blur(4px)"
+                  }}
+                  data-aos="fade-up"
+                  data-aos-anchor-placement="center-center"
+                  data-aos-duration="800"
+                >
+                  <Flex fullWidth center>
+                    <Link to="https://pagespeed.web.dev/analysis/https-my-projects-delta-orcin-vercel-app-me/vcos3fthy7?form_factor=desktop">
+                      <Text
+                        color="#DD5599"
+                        className={globalStyles.textKanitBold24} fontStyle={"italic"}
                         style={{
-                          backdropFilter: "blur(4px)"
+                          textDecoration: "underline",
+                          textTransform: "uppercase",
                         }}
                       >
-                        <Flex fullWidth center>
-                          <Link to="https://pagespeed.web.dev/analysis/https-my-projects-delta-orcin-vercel-app-me/vcos3fthy7?form_factor=desktop">
-                            <Text
-                              color="#DD5599"
-                              className={globalStyles.textKanitBold24} fontStyle={"italic"}
-                              style={{
-                                textDecoration: "underline",
-                                textTransform: "uppercase",
-                              }}
-                            >
-                              {getText("Page speed test result")}
-                            </Text>
-                          </Link>
-                        </Flex>
-
-                        <Flex fullWidth column={md1160} mt={4}>
-                          <Flex flex={1} center p={1}>
-                            <Text className={globalStyles.textKanitBold18}>
-                              {getText("Good page performance")}
-                            </Text>
-                          </Flex>
-                          <Flex flex={1} center p={1}>
-                            <Text className={globalStyles.textKanitBold18}>
-                              {getText("Best User Accessibility")}
-                            </Text>
-                          </Flex>
-                          <Flex flex={1} center p={1}>
-                            <Text className={globalStyles.textKanitBold18}>
-                              {getText("Best Coding practices")}
-                            </Text>
-                          </Flex>
-                          <Flex flex={1} center p={1}>
-                            <Text className={globalStyles.textKanitBold18}>
-                              {getText("Maximize SEO performance")}
-                            </Text>
-                          </Flex>
-                        </Flex>
-
-                        <Flex fullWidth mt={6}>
-                          <LazyImage
-                            src={pageSpeedRsImg} alt="page speed result"
-                          />
-                        </Flex>
-
-                      </Flex>
-                    </Animates.GrowUpAppear>
+                        {getText("Page speed test result")}
+                      </Text>
+                    </Link>
                   </Flex>
-                </IntersectionObserverView>
+
+                  <Flex fullWidth column={md1160} mt={4}>
+                    <Flex flex={1} center p={1}>
+                      <Text className={globalStyles.textKanitBold18}>
+                        {getText("Good page performance")}
+                      </Text>
+                    </Flex>
+                    <Flex flex={1} center p={1}>
+                      <Text className={globalStyles.textKanitBold18}>
+                        {getText("Best User Accessibility")}
+                      </Text>
+                    </Flex>
+                    <Flex flex={1} center p={1}>
+                      <Text className={globalStyles.textKanitBold18}>
+                        {getText("Best Coding practices")}
+                      </Text>
+                    </Flex>
+                    <Flex flex={1} center p={1}>
+                      <Text className={globalStyles.textKanitBold18}>
+                        {getText("Maximize SEO performance")}
+                      </Text>
+                    </Flex>
+                  </Flex>
+
+                  <Flex fullWidth mt={6}>
+                    <LazyImage
+                      src={pageSpeedRsImg} alt="page speed result"
+                      style={{
+                        borderRadius: "16px",
+                        boxShadow: "rgba(127, 229, 247, 0.74) 0 0 0.15em 0.15em",
+                      }}
+                    />
+                  </Flex>
+
+                </Flex>
               </Flex>
             </Flex>
           </Flex>
@@ -132,7 +142,7 @@ export const Products = observer(() => {
                 particles: 180,
                 speed: {
                   x: -3,
-                  y: 3,
+                  y: 9,
                   fade: 200,
                   acceleration: 200
                 }
@@ -141,17 +151,42 @@ export const Products = observer(() => {
           </Flex>}
         </Flex>
       </Flex>
-      {<Flex fullWidth height={400} position={"relative"} style={{
+
+      <Flex fullWidth height={350} position={"relative"}
+        className="water-meteor-bg"
+        style={{
+          filter: "blur(0px)",
+          fontSize: 40,
+        }}
+      >
+        <GAME.Seabed resources={{
+          audio: {
+            ping: pingAudio,
+            shoot: shootAudio,
+            water: waterAudio
+          },
+          images: {
+            cave: cavePng,
+            sonar: sonaPng,
+            clam: clamGif,
+            urchin: urchinGif,
+            whale: whaleGif,
+            sub: subGif,
+            wall: wall1Jpg
+          }
+        }} />
+      </Flex>
+
+      <Flex fullWidth height={400} position={"relative"} style={{
         pointerEvents: "none",
         opacity: 0.4,
-        transform: "translateY(-200px)",
       }}>
         <Animates.BgClouds cloudsImageUrl={[
           cloudBg1,
           cloudBg2,
           cloudBg3
         ]} />
-      </Flex>}
+      </Flex>
     </Flex>
   )
 })
---
+++ client/ui/react-animates/src/meteor/index.tsx
@@ -0,0 +1,5 @@
+import WaterMeteor from "./WaterMeteor";
+
+export namespace Meteor {
+  export const Water = WaterMeteor
+}
