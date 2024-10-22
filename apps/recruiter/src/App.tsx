a781fb9853e51bce755b03120ab44abc215c7747
raidenlight170 <thanhtai.tttgalaxy@gmail.com>
2024-10-23 02:01:53 +0700

    update

--- apps/recruiter/src/App.tsx
+++ apps/recruiter/src/App.tsx
@@ -3,28 +3,20 @@ import { AppLayout } from "@/layout/index"
 import "./reset.css";
 import "./index.css";
 import { AppStoreProvider } from "./store";
-import { JobsListingProvider } from "@core-ui/react-recruiter";
+import { RecruiterProvider } from "@core-ui/react-recruiter";
 
 export const App = ({ children }: { children: ReactNode }) => {
   return (
-    // <SocialProvider
-    //   config={{
-    //     useGoogle: true,
-    //     redirectUrl: import.meta.env.VITE_BASE_URL,
-    //     ggAppId: import.meta.env.VITE_GOOGLE_CLIENT_ID,
-    //   }}
-    // >
-      <AppStoreProvider>
-        <JobsListingProvider config={{
-          apiConfig: {
-            apiEndpoint: import.meta.env.VITE_API_URL
-          }
-        }}>
-          <AppLayout>
-            {children}
-          </AppLayout>
-        </JobsListingProvider>
-      </AppStoreProvider>
-    // </SocialProvider>
+    <AppStoreProvider>
+      <RecruiterProvider config={{
+        apiConfig: {
+          apiEndpoint: import.meta.env.VITE_API_URL
+        }
+      }}>
+        <AppLayout>
+          {children}
+        </AppLayout>
+      </RecruiterProvider>
+    </AppStoreProvider>
   )
 };
